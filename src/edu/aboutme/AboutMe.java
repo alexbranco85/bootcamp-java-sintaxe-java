29ee844a54f23cd6428e72d4aa66875871db5a36
alexbranco85 <alexbranco85@gmail.com>
2024-05-11 20:23:05 -0300

    Trabalhando com scanner no terminal

---
+++ src/edu/aboutme/AboutMe.java
@@ -0,0 +1,23 @@
+package edu.aboutme;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome:");
+        String nome = scanner.next();
+
+        System.out.println("Digite sua idade:");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura:");
+        double altura = scanner.nextDouble();
+
+        System.out.println("Meu nome é: " + nome);
+        System.out.println("Tenho: " + idade + " anos");
+        System.out.println("Minha altura: " + altura);
+    }
+}
